d47d1b87227f598c6906350dff2904b688400a16
BrunoAFonseca <brunoaf@outlook.com>
2022-07-05 12:49:10 -0300

    Componente progresso

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<TasksProgress :progress="progress"/>
 		<NewTask @taskAdded="addTask" />
 		<TaskGrid :tasks="tasks"
 			@taskDeleted="deleteTask" 
@@ -9,17 +10,22 @@
 </template>
 
 <script>
+import TasksProgress from './components/TasksProgress.vue'
 import TaskGrid from './components/TaskGrid.vue'
 import NewTask from './components/NewTask.vue'
 
 export default {
-	components: { TaskGrid, NewTask },
+	components: { TaskGrid, NewTask, TasksProgress },
 	data() {
 		return {
-			tasks: [
-				{name : 'Aspirar a casa', pending: false},
-				{name: 'Passar remédio no Odin', pending: true},
-			]
+			tasks: []
+		}
+	},
+	computed: {
+		progress() {
+			const total = this.tasks.length
+			const done = this.tasks.filter(t => !t.pending).length
+			return Math.round(done / total * 100) || 0
 		}
 	},
 	// Método para não repetir nomes nos cards!
